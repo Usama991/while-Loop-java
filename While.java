18f715d706138d9d65d02d9e8c700bcfb71caef1
Usama Zafar <90896659+Usama991@users.noreply.github.com>
2023-08-13 16:19:27 +0500

    Add files via upload

---
+++ While.java
@@ -0,0 +1,13 @@
+public class While {
+    public static void main(String[] args) {
+        int a = 12;
+        while (a!=16) {
+            System.out.println(a);
+            a++;
+            
+        }
+    }
+
+    
+
+}
